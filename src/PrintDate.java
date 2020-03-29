0edf817cf4452fc6d8280cfed9ba0e07a3ea6349
Haasi <haasitha123@gmail.com>
2020-03-28 21:27:31 -0700

    Update PrintDate.java

--- src/PrintDate.java
+++ src/PrintDate.java
@@ -1,47 +1,69 @@
+
 import java.util.*; 
 import java.text.*; 
+
 public class PrintDate{
 
+	final static Calendar cal = Calendar.getInstance();
+	final static SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
+ 
     public static void main(String args[]){
+    	boolean validDate = false;
+    	String input = "";
+
         // get the current dates from user
         Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter the current date");
-        System.out.print("Format is (MM/DD/YYYY): ");
-        String current = sc.nextLine();
-        nextDate(current);
-    } 
+        dateFormat.setLenient(false);
 
-    public static void nextDate(String date){ 
-        SimpleDateFormat dayFormat = new SimpleDateFormat("MM/dd/yyyy");
-        Calendar cal = Calendar.getInstance(); 
-        
-        try {
-            Date current = dayFormat.parse(date);     
-            cal.setTime(current); 
-            cal.add(Calendar.DAY_OF_YEAR, 1); 
-            String tomorrow  = dayFormat.format(cal.getTime());
+        while(!validDate) {
+	        try {
+	        	System.out.print("Enter a date (MM/DD/YYYY): ");
+	        	input = sc.nextLine();
+	        	Date date = dateFormat.parse(input);
 
-            boolean year = validYear(cal.get(Calendar.YEAR));
-
-            if(!year){
-                System.out.println("Next date is " + tomorrow ); 
-            }
-            else{
-                System.out.println("Invalid year"); 
-            }
-        } 
-        catch (ParseException e) {
-            System.out.println("Invalid input"); 
-        }
+	        	// check year
+				cal.setTime(date); 
+	        	boolean year = validYear(input);
+	        	if (year) { 
+	        		validDate = true; 
+	        	} 
+	        	else { 
+	        		System.out.println("Error: Not a valid year. Enter between 1900-2099!\n");
+	        		validDate = false; 
+	        	}
+	        
+	        } catch (ParseException e) {
+	        	System.out.println("Error: Invalid input\n"); 
+	        }
+	    }
+	    //System.out.println(date);
+        String next = nextDate(input);
+        System.out.println("You entered: " + input);
+        System.out.println("Next Day: " + next + "\n");
     }
+    
+    public static String nextDate(String input) { 
+        String tomorrow = "";
+        try {	
+        	Date date = dateFormat.parse(input);
+        	cal.setTime(date);    
+            
+            cal.add(Calendar.DAY_OF_YEAR, 1); 
+            tomorrow = dateFormat.format(cal.getTime());
+        } catch (ParseException e) {
+        	System.out.println("Error: Invalid input\n");
+        }
+        return tomorrow;
+    }
+
     // check the dates are between the year 1900 and 2099 inclusive
-    public static boolean validYear(int year){
-        boolean check = false;
-        if(year <= 1900 || year >= 2099){
-            return check = true;
+    public static boolean validYear(String date){
+        int year = cal.get(Calendar.YEAR);
+        if (year < 1900 || year > 2099) {
+            return false;
         }
         else{
-            return check;
+            return true;
         } 
     }
 }
